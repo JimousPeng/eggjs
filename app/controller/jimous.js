94895edc5b84a214ed8321cdb2294b9638f7dcd8
jimous <jimouspeng@xiaoe-tech.com>
2022-01-26 11:55:14 +0800

    测试配置

--- app/controller/jimous.js
+++ app/controller/jimous.js
@@ -2,7 +2,7 @@
  * @Date: 2022-01-25 17:51:46
  * @LastEditors: jimouspeng
  * @Description: 描述文件内容
- * @LastEditTime: 2022-01-25 17:53:52
+ * @LastEditTime: 2022-01-26 11:54:28
  * @FilePath: \eggjs\app\controller\jimous.js
  */
 'use strict';
@@ -12,7 +12,7 @@ const Controller = require('egg').Controller;
 class JimousController extends Controller {
   async index() {
     const { ctx } = this;
-    ctx.body = 'hi, jimous';
+    ctx.body = 'hi, 阿离';
     // 渲染模板 `app/view/home.tpl`
     // await ctx.render('home.tpl');
   }
